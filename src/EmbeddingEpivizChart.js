1f022276b748f1d6f07ce9e6452832454e34c159
Masood Syed <masood.syed@contractors.roche.com>
2021-09-07 09:03:35 -0400

    Epiviz Components related fixes

--- src/EmbeddingEpivizChart.js
+++ src/EmbeddingEpivizChart.js
@@ -19,14 +19,123 @@ class EmbeddingEpivizChart extends React.PureComponent {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            loadEpivizComponent: false
+          }
+
+          this.handleAddSecondInput = this.handleAddSecondInput.bind(this);
     }
 
-    componentWillUnmount() {
+    componentDidMount() {
+        console.log('I work,,', document.querySelector('#testDiv'));
+        // document.querySelector('#testDiv').innerHTML = '<epiviz-navigation chr="chr2" start="126998023" end="127207154" genome="hg38"'
+        //  + 'ms-api="https://gepiviz.science.roche.com/emd/api/v1/">'
+        // + '<epiviz-genes-track slot="charts" dim-s=' + '["hg38"]' + '></epiviz-genes-track>'
+        // + '<epiviz-stacked-blocks-track slot="charts"'
+        // + 'dim-s=' + '["catlas-human-tissues::peaks::Bly", "catlas-human-tissues::peaks::Bas.1", "catlas-human-tissues::peaks::Bas.2"]' + '>'
+        // + '</epiviz-stacked-blocks-track>';
 
+        // document.querySelector('#testDiv').innerHTML = '<epiviz-navigation chr="chr2" start="126998023" end="127207154" genome="hg38"'
+        //     + 'ms-api="https://gepiviz.science.roche.com/emd/api/v1/">'
+        //     + '<epiviz-genes-track slot="charts" dim-s=' + '["hg38"]' + '></epiviz-genes-track>'
+        //     + '<epiviz-stacked-blocks-track slot="charts" measurements=' + '['
+        //     + '{'
+        //     + '"id": "catlas-human-tissues::peaks::Bly",'
+        //     + '"name": "catlas-human-tissues::peaks::Bly",'
+        //     + '"type": "range",'
+        //     + '"datasourceId": "catlas-human-tissues::peaks::Bly",'
+        //     + '"datasourceGroup": "catlas-human-tissues::peaks::Bly",'
+        //     + '"dataprovider": "umd",'
+        //     + '"formula": null,'
+        //     + '"defaultChartType": "Blocks Track",'
+        //     + '"annotation": null,'
+        //     + '"minValue": null,'
+        //     + '"maxValue": null,'
+        //     + '"metadata": []'
+        //     + '},{'
+        //     + '"id": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"name": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"type": "range",'
+        //     + '"datasourceId": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"datasourceGroup": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"dataprovider": "umd",'
+        //     + '"formula": null,'
+        //     + '"defaultChartType": "Blocks Track",'
+        //     + '"annotation": null,'
+        //     + '"minValue": null,'
+        //     + '"maxValue": null,'
+        //     + '"metadata": []'
+        //     + '},{'
+        //     + '"id": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"name": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"type": "range",'
+        //     + '"datasourceId": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"datasourceGroup": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"dataprovider": "umd",'
+        //     + '"formula": null,'
+        //     + '"defaultChartType": "Blocks Track",'
+        //     + '"annotation": null,'
+        //     + '"minValue": null,'
+        //     + '"maxValue": null,'
+        //     + '"metadata": []'
+        //     + '}'
+        //     + ']' + '>'
+        //     + '</epiviz-stacked-blocks-track>'
+
+        //     + '<epiviz-multistacked-line-track slot="charts" measurements=' + '['
+        //     + '{'
+        //     + '"id": "catlas-human-tissues::peaks::Bly",'
+        //     + '"name": "catlas-human-tissues::peaks::Bly",'
+        //     + '"type": "range",'
+        //     + '"datasourceId": "catlas-human-tissues::peaks::Bly",'
+        //     + '"datasourceGroup": "catlas-human-tissues::peaks::Bly",'
+        //     + '"dataprovider": "umd",'
+        //     + '"formula": null,'
+        //     + '"defaultChartType": "Blocks Track",'
+        //     + '"annotation": null,'
+        //     + '"minValue": null,'
+        //     + '"maxValue": null,'
+        //     + '"metadata": []'
+        //     + '},{'
+        //     + '"id": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"name": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"type": "range",'
+        //     + '"datasourceId": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"datasourceGroup": "catlas-human-tissues::peaks::Bas.1",'
+        //     + '"dataprovider": "umd",'
+        //     + '"formula": null,'
+        //     + '"defaultChartType": "Blocks Track",'
+        //     + '"annotation": null,'
+        //     + '"minValue": null,'
+        //     + '"maxValue": null,'
+        //     + '"metadata": []'
+        //     + '},{'
+        //     + '"id": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"name": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"type": "range",'
+        //     + '"datasourceId": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"datasourceGroup": "catlas-human-tissues::peaks::Bas.2",'
+        //     + '"dataprovider": "umd",'
+        //     + '"formula": null,'
+        //     + '"defaultChartType": "Blocks Track",'
+        //     + '"annotation": null,'
+        //     + '"minValue": null,'
+        //     + '"maxValue": null,'
+        //     + '"metadata": []'
+        //     + '}'
+        //     + ']' + '></epiviz-multistacked-line-track>'
+
+        //     + '</epiviz-navigation>';
+    }
+
+    handleAddSecondInput() {
+        console.log(this);
+        this.setState({loadEpivizComponent:  true});
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-
+        
     }
 
     handleToggleLegend = (e) => {
@@ -35,58 +144,213 @@ class EmbeddingEpivizChart extends React.PureComponent {
 
     render() {
         //console.log(setChartOptions);
+        const myStyle = {
+            position: 'relative',
+            ':host #header': {
+              color: '#000000'
+            }
+          };
         return (
-            <div style={{ position: 'relative' }}>
+            
+            <div style={{ position: 'relative', color: 'black' }}>
+                
+                
+                <button type="button"className="make-button-link" data-add-button="" href="#" onClick={this.handleAddSecondInput}>
+                    Add component
+                </button>
 
-                <epiviz-data-source provider-type="epiviz.data.WebServerDataProvider" provider-id="umd" provider-url="https://epiviz-dev.cbcb.umd.edu/api/"></epiviz-data-source>
+                {this.state.loadEpivizComponent ? 
+                    <epiviz-navigation chr="chr2" start="126998023" end="127207154" genome="hg38"
+                        ms-api="https://gepiviz.science.roche.com/emd/api/v1/">
+                        <epiviz-genes-track slot="charts" dim-s='["hg38"]'></epiviz-genes-track>
+                        <epiviz-stacked-blocks-track slot="charts" measurements='[
+                        {
+                            "id": "catlas-human-tissues::peaks::Bly",
+                            "name": "catlas-human-tissues::peaks::Bly",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bly",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bly",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.1",
+                            "name": "catlas-human-tissues::peaks::Bas.1",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.1",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.1",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.2",
+                            "name": "catlas-human-tissues::peaks::Bas.2",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.2",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.2",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        }
+                        ]'>
+                        </epiviz-stacked-blocks-track>
 
-                <epiviz-environment chr="chr6" start={40076201} end={45076201} no-logo measurements='{"affy1":{
-                                    "id":"e070",
-                                    "name":"Expression Brain Germinal Matrix",
-                                    "type":"feature",
-                                    "datasourceId":"roadmap_rnaseq",
-                                    "datasourceGroup":"roadmap_rnaseq",
-                                    "dataprovider":"umd",
-                                    "formula":null,
-                                    "defaultChartType":null,
-                                    "annotation":null,
-                                    "minValue":-3,
-                                    "maxValue":20,
-                                    "metadata":["probe"]
-                                },
-                                "affy2":{
-                                    "id":"e071",
-                                    "name":"Expression Brain Hippocampus Middle",
-                                    "type":"feature",
-                                    "datasourceId":"roadmap_rnaseq",
-                                    "datasourceGroup":"roadmap_rnaseq",
-                                    "dataprovider":"umd",
-                                    "formula":null,
-                                    "defaultChartType":null,
-                                    "annotation":null,
-                                    "minValue":-3,
-                                    "maxValue":20,
-                                    "metadata":["probe"]
-                                },
-                                "genes": {
-                                    "id": "genes",
-                                    "name": "Genes",
-                                    "type": "range",
-                                    "datasourceId": "genes",
-                                    "datasourceGroup": "genes",
-                                    "dataprovider": "umd",
-                                    "formula": null,
-                                    "defaultChartType": "Genes Track",
-                                    "annotation": null,
-                                    "minValue": null,
-                                    "maxValue": null,
-                                    "metadata": ["gene", "exon_starts","exon_ends"]
-                                }
-                            }'>
-                    <epiviz-genes-track id="test" slot="charts" dim-s='["genes"]'></epiviz-genes-track>
-                    <epiviz-scatter-plot slot="charts" dim-s='["affy1", "affy2"]'></epiviz-scatter-plot>
-                </epiviz-environment>   
+                        <epiviz-multistacked-line-track slot="charts" measurements='[
+                        {
+                            "id": "catlas-human-tissues::peaks::Bly",
+                            "name": "catlas-human-tissues::peaks::Bly",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bly",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bly",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.1",
+                            "name": "catlas-human-tissues::peaks::Bas.1",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.1",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.1",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.2",
+                            "name": "catlas-human-tissues::peaks::Bas.2",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.2",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.2",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        }
+                        ]'></epiviz-multistacked-line-track>
 
+                    </epiviz-navigation>
+                : console.log('not loading..')}
+    
+
+
+                <epiviz-navigation chr="chr2" start="126998023" end="127207154" genome="hg38"
+                        ms-api="https://gepiviz.science.roche.com/emd/api/v1/">
+                        <epiviz-genes-track slot="charts" dim-s='["hg38"]'></epiviz-genes-track>
+                        <epiviz-stacked-blocks-track slot="charts" measurements='[
+                        {
+                            "id": "catlas-human-tissues::peaks::Bly",
+                            "name": "catlas-human-tissues::peaks::Bly",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bly",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bly",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.1",
+                            "name": "catlas-human-tissues::peaks::Bas.1",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.1",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.1",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.2",
+                            "name": "catlas-human-tissues::peaks::Bas.2",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.2",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.2",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        }
+                        ]'>
+                        </epiviz-stacked-blocks-track>
+
+                        <epiviz-multistacked-line-track slot="charts" measurements='[
+                        {
+                            "id": "catlas-human-tissues::peaks::Bly",
+                            "name": "catlas-human-tissues::peaks::Bly",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bly",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bly",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.1",
+                            "name": "catlas-human-tissues::peaks::Bas.1",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.1",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.1",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        },{
+                            "id": "catlas-human-tissues::peaks::Bas.2",
+                            "name": "catlas-human-tissues::peaks::Bas.2",
+                            "type": "range",
+                            "datasourceId": "catlas-human-tissues::peaks::Bas.2",
+                            "datasourceGroup": "catlas-human-tissues::peaks::Bas.2",
+                            "dataprovider": "umd",
+                            "formula": null,
+                            "defaultChartType": "Blocks Track",
+                            "annotation": null,
+                            "minValue": null,
+                            "maxValue": null,
+                            "metadata": []
+                        }
+                        ]'></epiviz-multistacked-line-track>
+
+                    </epiviz-navigation>
+
+
+
+
+                <div id="testDiv"></div>
             </div>);
 
     }
